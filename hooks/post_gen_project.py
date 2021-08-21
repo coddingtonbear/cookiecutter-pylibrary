e06bb39124f5bcd911ddab497cc0cd3765ae841e
Adam Coddington <me@adamcoddington.net>
2021-08-21 13:01:00 -0700

    Simplified cookiecutter.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -26,29 +26,6 @@ def unlink_if_exists(path):
         os.unlink(path)
 
 if __name__ == "__main__":
-{%- if cookiecutter.c_extension_test_pypi == 'yes' %}
-{%- if cookiecutter.test_matrix_separate_coverage == 'no' %}
-    warn("TODO: c_extension_test_pypi=yes will not work with test_matrix_separate_coverage=no for now.")
-    sys.exit(1)
-{%- endif %}
-{%- if cookiecutter.c_extension_support == 'no' %}
-    warn("""
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!!                                                                            !!
-!!      ERROR:                                                                !!
-!!                                                                            !!
-!!          c_extension_test_pypi=yes is designed to build and publish        !!
-!!          platform-specific wheels.                                         !!
-!!                                                                            !!
-!!          You have set c_extension_support=no, and that will make every     !!
-!!          test environment publish duplicated universal wheels.             !!
-!!                                                                            !!
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-""")
-    sys.exit(1)
-{%- endif %}
-{%- endif %}
-
 {% if cookiecutter.sphinx_docs == "no" %}
     shutil.rmtree('docs')
     os.unlink('.readthedocs.yml')
@@ -61,84 +38,24 @@ if __name__ == "__main__":
     os.unlink(join('src', '{{ cookiecutter.package_name }}', 'cli.py'))
 {% endif %}
 
-{%- if cookiecutter.test_matrix_configurator == 'no' %}
-    os.unlink(join('ci', 'templates', 'tox.ini'))
-{% endif %}
-{%- if cookiecutter.allow_tests_inside_package == 'no' %}
-    shutil.rmtree(join('src', '{{ cookiecutter.package_name }}', 'tests'))
-{% endif %}
-{%- if not (cookiecutter.c_extension_support == 'cffi' or cookiecutter.setup_py_uses_setuptools_scm == 'yes') %}
-    os.unlink('pyproject.toml')
-{% endif %}
-{%- if cookiecutter.c_extension_support == 'no' %}
-    os.unlink(join('src', '{{ cookiecutter.package_name }}', '{{ cookiecutter.c_extension_module }}.c'))
-    os.unlink(join('src', '{{ cookiecutter.package_name }}', '{{ cookiecutter.c_extension_module }}.pyx'))
-    os.unlink(join('src', '{{ cookiecutter.package_name }}', '{{ cookiecutter.c_extension_module }}_build.py'))
-{%- elif cookiecutter.c_extension_support == 'cffi' %}
-    os.unlink(join('src', '{{ cookiecutter.package_name }}', '{{ cookiecutter.c_extension_module }}.pyx'))
-{%- elif cookiecutter.c_extension_support == 'cython' %}
-    os.unlink(join('src', '{{ cookiecutter.package_name }}', '{{ cookiecutter.c_extension_module }}.c'))
-    os.unlink(join('src', '{{ cookiecutter.package_name }}', '{{ cookiecutter.c_extension_module }}_build.py'))
-    try:
-        subprocess.check_call(['tox', '-e', 'cythonize'])
-    except Exception:
-        subprocess.check_call([sys.executable, '-mtox', '-e', 'cythonize'])
-{%- else %}
-    os.unlink(join('src', '{{ cookiecutter.package_name }}', '{{ cookiecutter.c_extension_module }}.pyx'))
-    os.unlink(join('src', '{{ cookiecutter.package_name }}', '{{ cookiecutter.c_extension_module }}_build.py'))
-{%- endif %}
-
-    unlink_if_exists(join('ci', 'appveyor-with-compiler.cmd'))
-{%- if cookiecutter.appveyor == 'no' %}
-    os.unlink(join('ci', 'templates', '.appveyor.yml'))
-    unlink_if_exists('.appveyor.yml')
-{% endif %}
-    unlink_if_exists(join('ci', 'templates', 'appveyor.yml'))
-    unlink_if_exists('appveyor.yml')
-    unlink_if_exists(join('ci', 'appveyor-bootstrap.py'))
-
-{%- if cookiecutter.travis == 'no' %}
-    os.unlink(join('ci', 'templates', '.travis.yml'))
-    unlink_if_exists('.travis.yml')
-{% endif %}
-
 {%- if cookiecutter.repo_hosting == 'no' %}
     os.unlink('CONTRIBUTING.rst')
-{% endif %}
-
-{%- if cookiecutter.setup_py_uses_setuptools_scm == 'yes' %}
-    os.unlink('MANIFEST.in')
 {% endif %}
 
 {%- if cookiecutter.pre_commit == 'no' %}
     os.unlink('.pre-commit-config.yaml')
 {% endif %}
 
-{%- if cookiecutter.version_manager == 'bump2version' %}
-    os.unlink('tbump.toml')
-{%- elif cookiecutter.version_manager == 'tbump' %}
-    os.unlink('.bumpversion.cfg')
+{%- if cookiecutter.github_actions == 'no' %}
+    os.unlink('.github')
 {% endif %}
 
 {%- if cookiecutter.license == "no" %}
     os.unlink('LICENSE')
 {% endif %}
 
-    print("""
-################################################################################
-
-    Generating CI configuration ...
-""")
-    try:
-        subprocess.check_call(['tox', '-e', 'bootstrap', '--sitepackages'])
-    except Exception:
-        try:
-            subprocess.check_call([sys.executable, '-mtox', '-e', 'bootstrap', '--sitepackages'])
-        except Exception:
-            subprocess.check_call([sys.executable, join('ci', 'bootstrap.py')])
 
     print("""
-################################################################################
 ################################################################################
 
     You have succesfully created `{{ cookiecutter.repo_name }}`.
@@ -177,20 +94,6 @@ if __name__ == "__main__":
         ./ci/bootstrap.py
 
 """)
-
-{%- if cookiecutter.c_extension_test_pypi == 'yes' %}
-    note("""
-NOTE:
-
-    You are using the c_extension_test_pypi option.
-
-    Make sure you are setting TWINE_PASSWORD as a secret env variable in CI settings:
-    - https://ci.appveyor.com/project/{{ cookiecutter.repo_username }}/{{ cookiecutter.repo_name }}/settings/environment
-    - https://travis-ci.com{% if cookiecutter.repo_hosting == 'github.com' %}/github
-                           {%- elif cookiecutter.repo_hosting == 'gitlab.com' %}/gitlab
-                           {%- endif %}/{{ cookiecutter.repo_username }}/{{ cookiecutter.repo_name }}/settings
-""")
-{%- endif %}
 
     command_line_interface_bin_name = '{{ cookiecutter.command_line_interface_bin_name }}'
     while command_line_interface_bin_name.endswith('.py'):
--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -1,99 +1,18 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-{%- if cookiecutter.legacy_python == "yes" %}
 from __future__ import absolute_import
 from __future__ import print_function
-{%- endif %}
 
 import io
-{%- if cookiecutter.c_extension_support in ['yes', 'cython'] %}
-import os
-{%- if cookiecutter.c_extension_support == 'yes' %}
-import platform
-{%- endif -%}
-{%- endif -%}
-{%- if cookiecutter.repo_hosting_domain == "no" %}
-import os.path
-{%- endif %}
 import re
-{%- if cookiecutter.c_extension_support == 'cffi' %}
-import sys
-{%- endif %}
 from glob import glob
 from os.path import basename
 from os.path import dirname
 from os.path import join
-{%- if cookiecutter.c_extension_support not in ['no', 'cffi'] %}
-from os.path import relpath
-{%- endif %}
 from os.path import splitext
 
-{% if cookiecutter.c_extension_support not in ['no', 'cffi'] -%}
-from setuptools import Extension
-{% endif -%}
 from setuptools import find_packages
 from setuptools import setup
-{%- if cookiecutter.c_extension_support != 'no' %}
-{%- if cookiecutter.c_extension_optional == 'yes' %}
-from setuptools.command.build_ext import build_ext
-{%- endif %}
-{%- if cookiecutter.c_extension_support == 'cython' %}
-
-try:
-    # Allow installing package without any Cython available. This
-    # assumes you are going to include the .c files in your sdist.
-    import Cython
-except ImportError:
-    Cython = None
-{%- endif %}
-{%- endif %}
-{%- if cookiecutter.c_extension_support != 'no' %}
-{%- if cookiecutter.c_extension_support in ['yes', 'cython'] %}
-
-# Enable code coverage for C code: we can't use CFLAGS=-coverage in tox.ini, since that may mess with compiling
-# dependencies (e.g. numpy). Therefore we set SETUPPY_CFLAGS=-coverage in tox.ini and copy it to CFLAGS here (after
-# deps have been safely installed).
-if 'TOX_ENV_NAME' in os.environ and os.environ.get('SETUP_PY_EXT_COVERAGE') == 'yes'
-{%- if cookiecutter.c_extension_support == 'yes' %} and platform.system() == 'Linux'{% endif %}:
-{%- if cookiecutter.c_extension_support == 'cython' %}
-    CFLAGS = os.environ['CFLAGS'] = '-DCYTHON_TRACE=1'
-    LFLAGS = os.environ['LFLAGS'] = ''
-{%- elif cookiecutter.c_extension_support == 'yes' %}
-    CFLAGS = os.environ['CFLAGS'] = '-fprofile-arcs -ftest-coverage'
-    LFLAGS = os.environ['LFLAGS'] = '-lgcov'
-{%- endif %}
-else:
-    CFLAGS = ''
-    LFLAGS = ''
-{%- endif %}
-{%- if cookiecutter.c_extension_optional == 'yes' %}
-
-
-class optional_build_ext(build_ext):
-    """Allow the building of C extensions to fail."""
-    def run(self):
-        try:
-            build_ext.run(self)
-        except Exception as e:
-            self._unavailable(e)
-            self.extensions = []  # avoid copying missing files (it would fail).
-
-    def _unavailable(self, e):
-        print('*' * 80)
-        print('''WARNING:
-
-    An optional code optimization (C extension) could not be compiled.
-
-    Optimizations for this package will not be available!
-        ''')
-
-        print('CAUSE:')
-        print('')
-        print('    ' + repr(e))
-        print('*' * 80)
-{%- endif %}
-{%- endif %}
-
 
 def read(*names, **kwargs):
     with io.open(
@@ -105,15 +24,7 @@ def read(*names, **kwargs):
 
 setup(
     name='{{ cookiecutter.distribution_name }}',
-{%- if cookiecutter.setup_py_uses_setuptools_scm == 'yes' %}
-    use_scm_version={
-        'local_scheme': 'dirty-tag',
-        'write_to': 'src/{{ cookiecutter.package_name }}/_version.py',
-        'fallback_version': '{{ cookiecutter.version }}',
-    },
-{%- else %}
     version='{{ cookiecutter.version }}',
-{%- endif %}
 {%- if cookiecutter.license != "no" %}
     license='{{ {
         "BSD 2-Clause License": "BSD-2-Clause",
@@ -129,17 +40,12 @@ setup(
     }}',
 {%- endif %}
     description={{ '{0!r}'.format(cookiecutter.project_short_description).lstrip('ub') }},
-    long_description='%s\n%s' % (
-        re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.rst')),
-        re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))
+    long_description='%s' % (
+        re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.md'))
     ),
     author={{ '{0!r}'.format(cookiecutter.full_name).lstrip('ub') }},
     author_email={{ '{0!r}'.format(cookiecutter.email).lstrip('ub') }},
-{%- if cookiecutter.repo_hosting_domain == "no" %}
-    url='file://' + os.path.abspath(dirname(__file__)),
-{%- else %}
     url='https://{{ cookiecutter.repo_hosting_domain }}/{{ cookiecutter.repo_username }}/{{ cookiecutter.repo_name }}',
-{%- endif %}
     packages=find_packages('src'),
     package_dir={'': 'src'},
     py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
@@ -171,13 +77,8 @@ setup(
         'Operating System :: POSIX',
         'Operating System :: Microsoft :: Windows',
         'Programming Language :: Python',
-{%- if cookiecutter.legacy_python == "yes" %}
-        'Programming Language :: Python :: 2.7',
-{%- endif %}
         'Programming Language :: Python :: 3',
-{%- if cookiecutter.legacy_python == "no" %}
         'Programming Language :: Python :: 3 :: Only',
-{%- endif %}
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
@@ -189,9 +90,6 @@ setup(
         # 'Programming Language :: Python :: Implementation :: Jython',
         # 'Programming Language :: Python :: Implementation :: Stackless',
         'Topic :: Utilities',
-{%- if cookiecutter.pypi_disable_upload == "yes" %}
-        'Private :: Do Not Upload',
-{%- endif %}
     ],
 {%- if cookiecutter.repo_hosting_domain != "no" %}
     project_urls={
@@ -199,7 +97,7 @@ setup(
         'Documentation': '{{ cookiecutter.sphinx_docs_hosting }}',
         'Changelog': '{{ cookiecutter.sphinx_docs_hosting }}en/latest/changelog.html',
 {%- else %}
-        'Changelog': 'https://{{ cookiecutter.repo_hosting_domain }}/{{ cookiecutter.repo_username }}/{{ cookiecutter.repo_name }}/blob/master/CHANGELOG.rst',
+        'Changelog': 'https://{{ cookiecutter.repo_hosting_domain }}/{{ cookiecutter.repo_username }}/{{ cookiecutter.repo_name }}/blob/master/CHANGELOG.md',
 {%- endif %}
         'Issue Tracker': 'https://{{ cookiecutter.repo_hosting_domain }}/{{ cookiecutter.repo_username }}/{{ cookiecutter.repo_name }}/issues',
     },
@@ -207,18 +105,8 @@ setup(
     keywords=[
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
-{%- if cookiecutter.legacy_python == "yes" %}
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*',
-{%- else %}
     python_requires='>=3.6',
-{%- endif %}
     install_requires=[
-{%- if cookiecutter.command_line_interface == 'click' %}
-        'click',
-{%- endif %}
-{%- if cookiecutter.c_extension_support == 'cffi' %}
-        'cffi>=1.0.0',
-{%- endif %}
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
     ],
     extras_require={
@@ -226,58 +114,14 @@ setup(
         #   'rst': ['docutils>=0.11'],
         #   ':python_version=="2.6"': ['argparse'],
     },
-{%- set setup_requires_interior %}
-{%- if cookiecutter.test_runner == 'pytest' and cookiecutter.setup_py_uses_test_runner == 'yes' %}
-        'pytest-runner',{% endif %}
-{%- if cookiecutter.setup_py_uses_setuptools_scm == 'yes' %}
-        'setuptools_scm>=3.3.1{% if cookiecutter.legacy_python == 'yes' %},<6.0{% endif %}',{% endif %}
-{%- endset %}
-{%- if cookiecutter.c_extension_support == 'cython' %}
-    setup_requires=[{{ setup_requires_interior }}
-        'cython',
-    ] if Cython else [{{ setup_requires_interior }}
+    setup_requires=[
+        'pytest-runner',
     ],
-{%- elif cookiecutter.c_extension_support == 'cffi' %}
-    # We only require CFFI when compiling.
-    # pyproject.toml does not support requirements only for some build actions,
-    # but we can do it in setup.py.
-    setup_requires=[{{ setup_requires_interior }}
-        'cffi>=1.0.0',
-    ] if any(i.startswith('build') or i.startswith('bdist') for i in sys.argv) else [{{setup_requires_interior}}
-    ],
-{%- elif setup_requires_interior.strip() %}
-    setup_requires=[{{ setup_requires_interior }}
-    ],
-{%- endif -%}
 {%- if cookiecutter.command_line_interface != 'no' %}
     entry_points={
         'console_scripts': [
             '{{ cookiecutter.command_line_interface_bin_name }} = {{ cookiecutter.package_name }}.cli:main',
         ]
     },
-{%- endif %}
-{%- if cookiecutter.c_extension_support != 'no' -%}
-{%- if cookiecutter.c_extension_optional == 'yes' %}
-    cmdclass={'build_ext': optional_build_ext},
-{%- endif %}
-{%- if cookiecutter.c_extension_support == 'cffi' %}
-    cffi_modules=[i + ':ffi' for i in glob('src/*/_*_build.py')],
-{%- else %}
-    ext_modules=[
-        Extension(
-            splitext(relpath(path, 'src').replace(os.sep, '.'))[0],
-            sources=[path],
-{%- if cookiecutter.c_extension_support in ['yes', 'cython'] %}
-            extra_compile_args=CFLAGS.split(),
-            extra_link_args=LFLAGS.split(),
-{%- endif %}
-            include_dirs=[dirname(path)]
-        )
-        for root, _, _ in os.walk('src')
-        for path in glob(join(root,
-{%- if cookiecutter.c_extension_support == 'cython' %} '*.pyx' if Cython else '*.c'
-{%- else %} '*.c'{% endif %}))
-    ],
-{%- endif %}
 {%- endif %}
 )
